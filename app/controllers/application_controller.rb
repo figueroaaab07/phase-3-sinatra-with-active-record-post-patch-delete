c09a8b86333218791bd541a9f4dfb5f6fe0aee90
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-11 11:59:54 -0700

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -14,6 +16,26 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    review.to_json
+    # send a response with the deleted review as JSON
+  end
+
+  post '/reviews' do
+    review = Review.create(score: params[:score], comment: params[:comment], game_id: params[:game_id], user_id: params[:user_id])
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(score: params[:score], comment: params[:comment])
+    review.to_json
   end
 
 end
